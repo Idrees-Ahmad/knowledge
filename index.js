0241e22fb64d7566027052c57ece4cd0a1291a62
Idrees-Ahmad <idressahmad894@gmail.com>
2023-11-13 11:25:52 +0500

    second commit

--- index.js
+++ index.js
@@ -19,13 +19,52 @@
 //     console.log("v1 ", v1)
 // }
 
-{
-        var v1 = "string1"
-        console.log("v1 ", v1)
-        {
-            console.log("v1 ",v1)
-           var v1 = "string2"
-            console.log("v1 ",v1)
-        }
-        console.log("v1 ", v1)
-    }
+// {
+//         var v1 = "string1"
+//         console.log("v1 ", v1)
+//         {
+//             console.log("v1 ",v1)
+//            var v1 = "string2"
+//             console.log("v1 ",v1)
+//         }
+//         console.log("v1 ", v1)
+//     }
+
+
+
+
+
+// let a = 20;
+// let b = 30;
+// let c = 35;
+// if(a > b && a > c){
+//     console.log('a is greater than b')
+// }
+// else if(b > c){
+// console.log('b is greater')
+// }
+// else
+// console.log('c is greater')
+// let day = "sunday"
+// switch (day) {
+//     case 'monday':
+//         console.log('case 1 = today is monday')
+//         break;
+//         case 'tuesday':
+//             console.log('case 2 = today is monday')
+//             break;
+//             case 'sunday':
+//                 console.log(' case 3 = today is monday')
+//                 break;
+
+//     default:
+//         console.log('not exist')
+//         break;
+// }
+
+
+// type conversion
+
+let a = '1' ;
+let b = 1 ;
+console.log(a+b)
