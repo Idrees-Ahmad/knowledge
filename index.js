3c26734b88b69d2e6794855469ed4e7f3e99415a
Idrees-Ahmad <idressahmad894@gmail.com>
2023-11-13 10:25:41 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// {
+//     const v1 = "string1"
+//     {
+//         console.log("v1 ",v1)
+//         const v2 = "string2"
+//         console.log("v2 ",v2)
+//     }
+//     console.log("v1 ", v1)
+// }
+
+
+// {
+//     let v1 = "string1"
+//     {
+//         console.log("v1 ",v1)
+//        let v2 = "string2"
+//         console.log("v2 ",v2)
+//     }
+//     console.log("v1 ", v1)
+// }
+
+{
+        var v1 = "string1"
+        console.log("v1 ", v1)
+        {
+            console.log("v1 ",v1)
+           var v1 = "string2"
+            console.log("v1 ",v1)
+        }
+        console.log("v1 ", v1)
+    }
