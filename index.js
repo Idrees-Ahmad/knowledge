b99976290ec9b5ac85be5c327e0c01a8eaeea82d
Idrees-Ahmad <idressahmad894@gmail.com>
2023-11-14 11:28:11 +0500

    commited

--- index.js
+++ index.js
@@ -65,6 +65,79 @@
 
 // type conversion
 
-let a = '1' ;
-let b = 1 ;
-console.log(a+b)
+// let a = false && "dog"
+// console.log(a)
+
+// let a = [] && "dog"
+// console.log(a)
+
+// let a = " " && "dog"
+// console.log(a)
+
+// pass by value
+// let x = 10;
+// let y = 20;
+// function fun(a,b){
+// let c = a+b;
+// console.log(c)
+// }
+
+// fun(x , y)
+// x = 40;
+// console.log(x)
+
+
+// pass by refrence
+// function fun(a){
+//     let arr2 = a;
+   
+//   return arr2
+// }
+// let arr = [10,20,30];
+// let arr2 = fun(arr)
+// arr2[0] = 2000;
+// console.log('arr1 ', arr)
+// console.log('arr2 ', arr2)
+
+
+
+// Callback function, passed as a parameter in the higher order function
+// function callbackFunction(){
+//     console.log('I am  a callback function');
+// }
+
+// // higher order function
+// function higherOrderFunction(func){
+//     console.log('I am higher order function')
+//  func()
+// }
+
+//   higherOrderFunction(callbackFunction);
+
+
+
+let obj = [{
+    id:101,
+    name:'idrees',
+    gender: 'male',
+    obj:{
+        course:'mern',
+        time: 9
+    }
+
+},
+{
+    id:101,
+    name:'idrees',
+    gender: 'male',
+    obj:[{
+        course:'mern',
+        time: 9
+    },
+{
+    course:'cyber',
+    time: 10   
+}]
+}]
+
+console.log(obj[0].obj.course)
